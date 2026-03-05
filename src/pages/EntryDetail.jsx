9d1fe8818afa533136aaf2f011847fdc33f12399
mohammed k <msm.kasid@gmail.com>
2026-03-05 06:37:05 -0700

    Fix import

--- src/pages/EntryDetail.jsx
+++ src/pages/EntryDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "../utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
