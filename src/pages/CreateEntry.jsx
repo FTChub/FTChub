0ba7e4baec0f4cc6f082f4fcfe08e23e1b38619b
mohammed k <msm.kasid@gmail.com>
2026-03-05 06:42:35 -0700

    Fix file name typo

--- src/pages/CreateEntry.jsx
+++ src/pages/CreateEntry.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "../utils";
+import { createPageUrl } from "@/utils";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
